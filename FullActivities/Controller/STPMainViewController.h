a259ce762a7df72a700c29586617dd1d764f219f
SaveThePlan <realnanook@gmail.com>
2014-06-15 15:25:54 +0200

    First Commit

---
+++ FullActivities/Controller/STPMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  STPMainViewController.h
+//  FullActivities
+//
+//  Created by Nanook on 15/06/2014.
+//  Copyright (c) 2014 SaveThePlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STPMainViewController : UIViewController
+
+@end
---
+++ FullActivities/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FullActivities
+//
+//  Created by Nanook on 15/06/2014.
+//  Copyright (c) 2014 SaveThePlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STPAppDelegate class]));
+    }
+}
